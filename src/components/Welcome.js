8ed3032f2897d1c6e94a8fc44c4325876d4fa189
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-11 16:18:02 +0530

    react assign

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,14 @@
 // write code for Welcome component here
+import React from "react";
+
+export default class welcome extends React.component {
+    rener() {
+        return (
+            <div>
+                <h1> Hey {this.props.name}!</h1>
+                <h2>Welcome to Newton School. </h2>
+
+            </div>
+        )
+    };
+}
